6807ca9122245b4768a7ff62b4b61e55917a0cb0
qinpmc <850823973@qq.com>
2019-11-14 17:27:48 +0800

    add redux demo1

---
+++ redux_demo1/src/TodoList.js
@@ -0,0 +1,40 @@
+import React,{Component} from "react"
+import "antd/dist/antd.css"
+import { Input , Button , List } from 'antd'
+import store from "./store/index"
+
+/* const data=[
+    '早8点开晨会，分配今天的开发工作',
+    '早9点和项目经理作开发需求讨论会',
+    '晚5:30对今日代码进行review'
+] */
+
+
+class TodoList extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = store.getState();
+        console.log(store.getState());
+    }
+
+    render(){
+        return (
+            <div style={{margin:"10px"}}>
+                <div>
+                    <Input placeholder={this.state.inputValue} style={{width:"250px",marginRight:"10px"}} />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{margin:"10px",width:"300px"}}>
+                <List 
+                    bordered
+                    dataSource={this.state.list}
+                    renderItem={item=>(<List.Item>{item}</List.Item>)}
+                />
+            </div>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
---
+++ redux_demo1/src/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+    inputValue:"something",
+    list:[
+        '早上',
+        '中午'
+    ]
+}
+
+export default (state = defaultState,action) => {
+    return state;
+}
